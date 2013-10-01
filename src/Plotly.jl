123ba38b73599c0ea19e0cde09295947bd956581
Shilei Zheng <shirlenator@gmail.com>
2013-10-01 02:43:20 -0400

    * Added signup function * Make PlotlyAccount a global variable and remove as plot function argument * Convert HTTP response body to bytestring

--- src/Plotly.jl
+++ src/Plotly.jl
@@ -7,10 +7,11 @@ type PlotlyAccount
   api_key::String
 end
 
-default_options = ["filename"=>"plot from api",
-                   "world_readable"=>true,
-                   "fileopt"=>"overwrite",
-                   "layout"=>["title"=>"experimental data"]]
+signedin = false
+
+default_options = ["filename"=>"Plot from API",
+                   "world_readable"=> true,
+                   "layout"=>["title"=>"Add Title in plotly"]]
 
 ## Taken from https://github.com/johnmyleswhite/Vega.jl/blob/master/src/Vega.jl#L51
 # Open a URL in a browser
@@ -25,22 +26,50 @@ default_opts = [
        "platform" => "julia",
        "version" => "0.1"]
 
-function plot(p::PlotlyAccount,data::Array,options=Dict())
+function signup(username::String, email::String)
+  r = HTTPClient.HTTPC.post("http://plot.ly/apimkacct", 
+                            merge(default_opts, 
+                                   ["un" => username, 
+                                    "email" => email]))
+  if r.http_code == 200
+    results = JSON.parse(bytestring(r.body)) 
+    for flag in ["error","warning","message"]
+      if haskey(results, flag) && results[flag] != ""
+        println(results[flag])
+      end
+    end
+    if !haskey(results,"error") && haskey(results,"tmp_pw")
+      println("Success! Check your email to activate your account.")
+    end
+  end
+end
+    
+function signin(username::String, api_key::String)
+  global plotlyaccount 
+  plotlyaccount = PlotlyAccount(username,api_key)
+end
+
+function plot(data::Array,options=Dict())
+  global plotlyaccount
+  if !isdefined(Plotly,:plotlyaccount)
+    println("Please 'signin(username, api_key)' before proceeding. See http://plot.ly/API for help!")
+    return
+  end
   opt = merge(default_options,options)
   r = HTTPClient.HTTPC.post("http://plot.ly/clientresp", 
         merge(default_opts,
-          ["un" => p.username,
-           "key" => p.api_key,
+          ["un" => plotlyaccount.username,
+           "key" => plotlyaccount.api_key,
            "args" => json(data),
            "kwargs" => json(opt)]))
   if r.http_code == 200
-    b = JSON.parse(r.body)
+    b=JSON.parse(bytestring(r.body))
     b["error"] == "" ? b["url"] : error(b["error"]) 
   else
     error(r.http_code)
   end
 end
 
-plot(p::PlotlyAccount,data::Dict,options=Dict()) = plot(p,push!(Dict[],data),options)
+plot(data::Dict,options=Dict()) = plot(push!(Dict[],data),options)
 
 end
