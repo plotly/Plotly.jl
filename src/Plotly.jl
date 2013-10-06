f43511337ef553c6099e22d107ad9fa06a797e72
Shilei Zheng <shirlenator@gmail.com>
2013-10-06 02:26:04 -0400

    layout and style

--- src/Plotly.jl
+++ src/Plotly.jl
@@ -7,11 +7,15 @@ type PlotlyAccount
   api_key::String
 end
 
-signedin = false
+type CurrentPlot
+  filename::String
+  fileopt::String
+  url::String
+end
 
-default_options = ["filename"=>"Plot from API",
+default_options = ["filename"=>"Plot from Julia API",
                    "world_readable"=> true,
-                   "layout"=>["title"=>"Add Title in plotly"]]
+                   "layout"=>["title"=>"Plot from Julia API"]]
 
 ## Taken from https://github.com/johnmyleswhite/Vega.jl/blob/master/src/Vega.jl#L51
 # Open a URL in a browser
@@ -23,7 +27,7 @@ end
 
 default_opts = [ 
        "origin" => "plot",
-       "platform" => "julia",
+       "platform" => "Julia",
        "version" => "0.1"]
 
 function signup(username::String, email::String)
@@ -64,7 +68,74 @@ function plot(data::Array,options=Dict())
            "args" => json(data),
            "kwargs" => json(opt)]))
   body=JSON.parse(bytestring(r.body))
-  if r.http_code != 200 
+  if r.http_code != 200
+    error(["r.http_code"])
+  elseif body["error"] != ""
+    error(body["error"])
+  else
+    global currentplot
+    currentplot=CurrentPlot(body["filename"],"new",body["url"])
+    body
+  end
+end
+
+function layout(layout_opts::Dict,meta_opts=Dict())
+  global plotlyaccount
+  if !isdefined(Plotly,:plotlyaccount)
+    println("Please 'signin(username, api_key)' before proceeding. See http://plot.ly/API for help!")
+    return
+  end
+  
+  merge!(meta_opts,get_required_params(["filename","fileopt"],meta_opts))
+
+  r = HTTPClient.HTTPC.post("http://plot.ly/clientresp",
+        merge(default_opts,
+          ["un" => plotlyaccount.username,
+           "key" => plotlyaccount.api_key,
+           "args" => json(layout_opts),
+           "origin" => "layout",
+           "kwargs" => json(meta_opts)]))
+  __parseresponse(r)
+end
+
+function style(style_opts,meta_opts=Dict())
+  global plotlyaccount
+  if !isdefined(Plotly,:plotlyaccount)
+    println("Please 'signin(username, api_key)' before proceeding. See http://plot.ly/API for help!")
+    return
+  end
+  
+  merge!(meta_opts,get_required_params(["filename","fileopt"],meta_opts))
+  
+  r = HTTPClient.HTTPC.post("http://plot.ly/clientresp",
+        merge(default_opts,
+          ["un" => plotlyaccount.username,
+           "key" => plotlyaccount.api_key,
+           "args" => json(["style" =>style_opts]),
+           "origin" => "style",
+           "kwargs" => json(meta_opts)]))
+  __parseresponse(r)
+end
+  
+function get_required_params(required,opts)
+  # Priority given to user-inputted opts, then currentplot
+  result=Dict()
+  for p in required 
+    global currentplot
+    if haskey(opts,p)
+      result[p] = opts[p]
+    elseif isdefined(Plotly,:currentplot)
+      result[p] = getfield(currentplot,symbol(p))
+    else
+      error("Missing required param ",p, ". Make sure to create a plot first. Please refer to http://plot.ly/api, or ask chris@plot.ly")
+    end
+  end
+  result
+end
+
+function __parseresponse(r)
+  body=JSON.parse(bytestring(r.body))
+  if r.http_code != 200
     error(["r.http_code"])
   elseif body["error"] != ""
     error(body["error"])
@@ -72,7 +143,5 @@ function plot(data::Array,options=Dict())
     body
   end
 end
-
-plot(data::Dict,options=Dict()) = plot(push!(Dict[],data),options)
 
 end
