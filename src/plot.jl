5c4739bdf3b8f7db3ae8be1fc3b7edf6a9f58b54
Bryan A. Knowles <snotskie@gmail.com>
2014-06-10 17:09:39 -0500

    Added support for direct plotting of math functions

--- src/plot.jl
+++ src/plot.jl
@@ -1,23 +1,34 @@
 pkgs = keys(Pkg.installed())
 
+function get_points(f::Function, options=Dict())
+	opt = merge(["left"=>-10, "right"=>10, "step"=>0.5, "name"=>"$f"], options)
+	n::Int = (opt["right"] - opt["left"]) / opt["step"] + 1
+	X = Float64[0 for i in 1:n]
+	Y = Float64[0 for i in 1:n]
+	for i in 1:n
+		x = opt["step"]*(i-1) + opt["left"]
+		y = f(x)
+		X[i] = round(x, 8)
+		Y[i] = round(y, 8)
+	end
+	print(["x"=>X, "y"=>Y, "type"=>"scatter", "mode"=>"lines", "name"=>opt["name"]])
+	return ["x"=>X, "y"=>Y, "type"=>"scatter", "mode"=>"lines", "name"=>opt["name"]]
+end
+
+function plot(fs::Array{Function,1}, options=Dict())
+	data = [get_points(f, options) for f in fs]
+	return plot([data], options)
+end
+
+function plot(f::Function, options=Dict())
+	return plot([f], options)
+end
+
 if in("Polynomial", pkgs)
 	using Polynomial
-	function get_points(p::Poly, left, right, step)
-		n::Int = (right - left) / step + 1
-		X = Float64[0 for i in 1:n]
-		Y = Float64[0 for i in 1:n]
-		for i in 1:n
-			x = step*(i-1) + left
-			y = polyval(p, x)
-			X[i] = x
-			Y[i] = y
-		end
-		return ["x"=>X, "y"=>Y, "type"=>"scatter", "mode"=>"lines", "name"=>"$p"]
-	end
 
 	function plot{T<:Number}(ps::Array{Poly{T},1}, options=Dict())
-		opt = merge(options, ["left"=>-10, "right"=>10, "step"=>0.5])
-		data = [get_points(p, opt["left"], opt["right"], opt["step"]) for p in ps]
+		data = [get_points(x->polyval(p,x), merge(["name"=>"$p"], options)) for p in ps]
 		return plot([data], options)
 	end
 
