ea6534809471aa475f7708b79ca3c8359dc0abe7
Spencer Lyon <spencerlyon2@gmail.com>
2016-03-14 15:39:53 -0400

    @reexport PlotlyJS, Purge src/plots.js

--- src/Plotly.jl
+++ src/Plotly.jl
@@ -1,10 +1,12 @@
-__precompile__(true)
+# __precompile__(true)
 
 module Plotly
 using Requests
 using JSON
+using Reexport: @reexport
 
-include("plot.jl")
+@reexport using PlotlyJS
+
 include("utils.jl")
 
 #export default_options, default_opts, get_config, get_plot_endpoint, get_credentials,get_content_endpoint,get_template
@@ -15,11 +17,11 @@ type CurrentPlot
     url::ASCIIString
 end
 
-api_version = "v2"
+const api_version = "v2"
 
-default_options = Dict("filename"=>"Plot from Julia API",
-  "world_readable"=> true,
-  "layout"=>Dict(""=>""))
+const default_options = Dict("filename"=>"Plot from Julia API",
+                             "world_readable"=> true,
+                             "layout"=>Dict())
 
 ## Taken from https://github.com/johnmyleswhite/Vega.jl/blob/master/src/Vega.jl#L51
 # Open a URL in a browser
@@ -29,32 +31,23 @@ function openurl(url::ASCIIString)
     @linux_only run(`xdg-open $url`)
 end
 
-default_opts = Dict(
-  "origin" => "plot",
-  "platform" => "Julia",
-  "version" => "0.2")
+const default_opts = Dict("origin" => "plot",
+                          "platform" => "Julia",
+                          "version" => "0.2")
 
-function get_plot_endpoint()
-    config = get_config()
-    plot_endpoint = "clientresp"
-    return "$(config.plotly_domain)/$plot_endpoint"
-end
+get_plot_endpoint() = "$(get_config().plotly_domain)/clientresp"
 
 function get_content_endpoint(file_id::ASCIIString, owner::ASCIIString)
     config = get_config()
     api_endpoint = "$(config.plotly_api_domain)/$api_version/files"
     detail = "$owner:$file_id"
-    custom_action = "content"
-    content_endpoint = "$api_endpoint/$detail/$custom_action"
-    return content_endpoint
+    "$api_endpoint/$detail/content"
 end
 
 function plot(data::Array,options=Dict())
     creds = get_credentials()
     endpoint = get_plot_endpoint()
     opt = merge(default_options,options)
-
-    #post("http://httpbin.org/post"; headers = Dict("Date" => "Tue, 15 Nov 1994 08:12:31 GMT"), cookies = Dict("sessionkey" => "abc"))
 
     r = post(endpoint,
              data = merge(default_opts,
@@ -162,101 +155,6 @@ function __parseresponse(r)
     else
         body
     end
-end
-
-function get_template(format_type::ASCIIString)
-    if format_type == "layout"
-        return Dict(
-                "title"=>"Click to enter Plot title",
-                "xaxis"=>Dict(
-                        "range"=>[-1,6],
-                        "type"=>"-",
-                        "mirror"=>true,
-                        "linecolor"=>"#000",
-                        "linewidth"=>1,
-                        "tick0"=>0,
-                        "dtick"=>2,
-                        "ticks"=>"outside",
-                        "ticklen"=>5,
-                        "tickwidth"=>1,
-                        "tickcolor"=>"#000",
-                        "nticks"=>0,
-                        "showticklabels"=>true,
-                        "tickangle"=>"auto",
-                        "exponentformat"=>"e",
-                        "showexponent"=>"all",
-                        "showgrid"=>true,
-                        "gridcolor"=>"#ddd",
-                        "gridwidth"=>1,
-                        "autorange"=>true,
-                        "autotick"=>true,
-                        "zeroline"=>true,
-                        "zerolinecolor"=>"#000",
-                        "zerolinewidth"=>1,
-                        "title"=>"Click to enter X axis title",
-                        "unit"=>"",
-                        "titlefont"=>Dict("family"=>"","size"=>0,"color"=>""),
-                        "tickfont"=>Dict("family"=>"","size"=>0,"color"=>"")),
-                "yaxis"=>Dict(
-                        "range"=>[-1,4],
-                        "type"=>"-",
-                        "mirror"=>true,
-                        "linecolor"=>"#000",
-                        "linewidth"=>1,
-                        "tick0"=>0,
-                        "dtick"=>1,
-                        "ticks"=>"outside",
-                        "ticklen"=>5,
-                        "tickwidth"=>1,
-                        "tickcolor"=>"#000",
-                        "nticks"=>0,
-                        "showticklabels"=>true,
-                        "tickangle"=>"auto",
-                        "exponentformat"=>"e",
-                        "showexponent"=>"all",
-                        "showgrid"=>true,
-                        "gridcolor"=>"#ddd",
-                        "gridwidth"=>1,
-                        "autorange"=>true,
-                        "autotick"=>true,
-                        "zeroline"=>true,
-                        "zerolinecolor"=>"#000",
-                        "zerolinewidth"=>1,
-                        "title"=>"Click to enter Y axis title",
-                        "unit"=>"",
-                        "titlefont"=>Dict("family"=>"","size"=>0,"color"=>""),
-                        "tickfont"=>Dict("family"=>"","size"=>0,"color"=>"")),
-                "legend"=>Dict(
-                        "bgcolor"=>"#fff",
-                        "bordercolor"=>"#000",
-                        "borderwidth"=>1,
-                        "font"=>Dict("family"=>"","size"=>0,"color"=>""),
-                        "traceorder"=>"normal"),
-                "width"=>700,
-                "height"=>450,
-                "autosize"=>"initial",
-                "margin"=>Dict("l"=>80,"r"=>80,"t"=>80,"b"=>80,"pad"=>2),
-                "paper_bgcolor"=>"#fff",
-                "plot_bgcolor"=>"#fff",
-                "barmode"=>"stack",
-                "bargap"=>0.2,
-                "bargroupgap"=>0.0,
-                "boxmode"=>"overlay",
-                "boxgap"=>0.3,
-                "boxgroupgap"=>0.3,
-                "font"=>Dict("family"=>"Arial, sans-serif;","size"=>12,"color"=>"#000"),
-                "titlefont"=>Dict("family"=>"","size"=>0,"color"=>""),
-                "dragmode"=>"zoom",
-                "hovermode"=>"x")
-    end
-end
-
-function help()
-    println("Please enter the name of the funtion you'd like help with")
-    println("Options include:")
-    println("\t Plotly.help(\"plot\") OR Plotly.help(:plot)")
-    println("\t Plotly.help(\"layout\") OR Plotly.help(:layout)")
-    println("\t Plotly.help(\"style\") OR Plotly.help(:style)")
 end
 
 end
