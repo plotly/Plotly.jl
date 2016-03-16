3fd28b7913e144a0c8692041d430629ab6aef71d
Jon Malmaud <malmaud@gmail.com>
2016-03-16 14:16:44 -0400

    Various improvements:
    * Added RemotePlot type to represent a plot.ly-hosted plot
    * download(::RemotePlot) retrieves the remote plot
    * General cleanup

--- src/Plotly.jl
+++ src/Plotly.jl
@@ -1,26 +1,25 @@
 # __precompile__(true)
 
 module Plotly
+using Compat
 using Requests
 using JSON
 using Reexport: @reexport
+import Requests: URI, post
 
 @reexport using PlotlyJS
+export post
 
 include("utils.jl")
-
-#export default_kwargs, default_opts, get_config, get_plot_endpoint, get_credentials,get_content_endpoint,get_template
-
-type CurrentPlot
-    filename::ASCIIString
-    fileopt::ASCIIString
-    url::ASCIIString
-end
 
 const api_version = "v2"
 
 const default_kwargs = Dict{Symbol,Any}(:filename=>"Plot from Julia API",
-                                         :world_readable=> true)
+                                        :world_readable=> true)
+
+const default_opts = Dict{Symbol,Any}(:origin => "plot",
+                                      :platform => "Julia",
+                                      :version => "0.2")
 
 ## Taken from https://github.com/johnmyleswhite/Vega.jl/blob/master/src/Vega.jl#L51
 # Open a URL in a browser
@@ -30,43 +29,34 @@ function openurl(url::ASCIIString)
     @linux_only run(`xdg-open $url`)
 end
 
-const default_opts = Dict{Symbol,Any}(:origin => "plot",
-                                      :platform => "Julia",
-                                      :version => "0.2")
+openurl(url::URI) = openurl(string(url))
 
 get_plot_endpoint() = "$(get_config().plotly_domain)/clientresp"
 
-function get_content_endpoint(file_id::ASCIIString, owner::ASCIIString)
-    config = get_config()
-    api_endpoint = "$(config.plotly_api_domain)/$api_version/files"
-    detail = "$owner:$file_id"
-    "$api_endpoint/$detail/content"
-end
 
-function Requests.post(p::Plot; kwargs...)
+immutable RemotePlot
+    url::URI
+end
+RemotePlot(url) = RemotePlot(URI(url))
+
+Base.open(p::RemotePlot) = openurl(p.url)
+
+function post(p::Plot; kwargs...)
     creds = get_credentials()
     endpoint = get_plot_endpoint()
     opt = merge(default_kwargs, Dict(:layout => p.layout.fields),
-                Dict(kwargs))
+    Dict(kwargs))
 
     data = merge(default_opts,
-                 Dict("un" => creds.username,
-                      "key" => creds.api_key,
-                      "args" => json(p.data),
-                      "kwargs" => json(opt)))
+    Dict("un" => creds.username,
+    "key" => creds.api_key,
+    "args" => json(p.data),
+    "kwargs" => json(opt)))
 
     r = post(endpoint, data=data)
-    body=Requests.json(r)
-
-    if statuscode(r) != 200
-        error(["r.status"])
-    elseif body["error"] != ""
-        error(body["error"])
-    else
-        global currentplot
-        currentplot=CurrentPlot(body["filename"], "new", body["url"])
-        body
-    end
+    # body=Requests.json(r)
+    body = parse_response(r)
+    return RemotePlot(URI(body["url"]))
 end
 
 function Requests.post(l::AbstractLayout, meta_opts=Dict(); meta_kwargs...)
@@ -74,59 +64,57 @@ function Requests.post(l::AbstractLayout, meta_opts=Dict(); meta_kwargs...)
     endpoint = get_plot_endpoint()
 
     meta = merge(meta_opts,
-                 get_required_params(["filename", "fileopt"], meta_opts),
-                 Dict(meta_kwargs))
+    get_required_params(["filename", "fileopt"], meta_opts),
+    Dict(meta_kwargs))
     data = merge(default_opts,
-                 Dict("un" => creds.username,
-                      "key" => creds.api_key,
-                      "args" => json(l),
-                      "origin" => "layout",
-                      "kwargs" => json(meta)))
+    Dict("un" => creds.username,
+         "key" => creds.api_key,
+         "args" => json(l),
+         "origin" => "layout",
+         "kwargs" => json(meta)))
 
-    __parseresponse(post(endpoint, data=data))
+    parse_response(post(endpoint, data=data))
 end
+
+post(p::PlotlyJS.SyncPlot) = post(p.plot)
 
 function style(style_opts, meta_opts=Dict(); meta_kwargs...)
     creds = get_credentials()
     endpoint = get_plot_endpoint()
 
     meta = merge(meta_opts,
-                 get_required_params(["filename", "fileopt"], meta_opts),
-                 Dict(meta_kwargs))
+    get_required_params(["filename", "fileopt"], meta_opts),
+    Dict(meta_kwargs))
     data = merge(default_opts,
-                 Dict("un" => creds.username,
-                      "key" => creds.api_key,
-                      "args" => json([style_opts]),
-                      "origin" => "style",
-                      "kwargs" => json(meta_opts)))
+    Dict("un" => creds.username,
+    "key" => creds.api_key,
+    "args" => json([style_opts]),
+    "origin" => "style",
+    "kwargs" => json(meta_opts)))
 
-    __parseresponse(post(endpoint, data=data))
+    parse_response(post(endpoint, data=data))
 end
 
 
-function getFile(file_id::ASCIIString, owner=None)
+function Base.download(plot::RemotePlot)
     creds = get_credentials()
     username = creds.username
     api_key = creds.api_key
-
-    if (owner == None)
-        owner = username
-    end
-
-    endpoint = get_content_endpoint(file_id, owner)
-    lib_version = string(default_opts["platform"], " ", default_opts["version"])
-
-    auth = string("Basic ", base64("$username:$api_key"))
-
+    lib_version = string(default_opts[:platform], " ", default_opts[:version])
+    auth = string("Basic ", base64encode("$username:$api_key"))
     options = Dict("Authorization"=>auth, "Plotly-Client-Platform"=>lib_version)
-
-    r = get(endpoint, headers=options)
-    print(r)
-
-    __parseresponse(r)
-
+    original_path = plot.url.path
+    if original_path[end] == '/'
+        path = original_path[1:end-1]
+    else
+        path = original_path
+    end
+    endpoint = URI(plot.url, path="$path.json")
+    response = get(endpoint, headers=options)
+    return JSON.parse(Plot, bytestring(response))
 end
 
+download_plot(url) = download(RemotePlot(url))
 
 function get_required_params(required,opts)
     # Priority given to user-inputted opts, then currentplot
@@ -139,22 +127,30 @@ function get_required_params(required,opts)
             result[p] = getfield(currentplot,symbol(p))
         else
             msg = string("Missing required param $(p). ",
-                         "Make sure to create a plot first. ",
-                         " Please refer to http://plot.ly/api")
+            "Make sure to create a plot first. ",
+            " Please refer to http://plot.ly/api")
             error(msg)
         end
     end
     result
 end
 
-function __parseresponse(r)
-    body=Requests.json(r)
-    if statuscode(r) != 200
-        error(["r.status"])
-    elseif haskey(body, "error") && body["error"] != ""
-        error(body["error"])
-    elseif haskey(body, "detail") && body["detail"] != ""
-        error(body["detail"])
+immutable PlotlyError <: Exception
+    msg::UTF8String
+end
+
+function Base.show(io::IO, err::PlotlyError)
+    print(io, "Plotly error: $(err.msg)")
+end
+
+function parse_response(r)
+    body = Requests.json(r)
+    if statuscode(r) ≠ 200
+        throw(PlotlyError("Non-sucessful status code: $(statuscode(r))"))
+    elseif "error" ∈ keys(body) && body["error"] ≠ ""
+        throw(PlotlyError(body["error"]))
+    elseif "detail" ∈ keys(body) && body["detail"] ≠ ""
+        throw(PlotlyError(body["detail"]))
     else
         body
     end
